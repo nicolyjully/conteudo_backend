0f9a817a7027aad7392c51d8c79c470126e76480
nicolyjully <nicoly.jully@alunos.ifsuldeminas.edu.br>
2023-03-08 10:47:34 -0300

    aula6

--- aula6/repeticao_while.php
+++ aula6/repeticao_while.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <h2>Estrutura de repetição While</h2>
 <hr>
 <p>
@@ -16,4 +17,24 @@
         echo "Olá pessoal $contador<br>";
         $contador++;
     }
+=======
+<h2>Estrutura de repetição While</h2>
+<hr>
+<p>
+    Utilizada para repetir um conjunto de instruções.
+</p>
+<strong>Exemplo</strong>
+<pre>
+    while(condição){
+        //comandos a serem repetidos.
+    }
+</pre>
+
+<?php
+    $contador = 0;
+    while($contador = 10){
+        echo "Olá pessoal $contador<br>";
+        $contador++;
+    }
+>>>>>>> fa875fe082ccd9b88e3ca6dc9a1535ceb3a32603
 ?>
